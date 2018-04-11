f229402fb52e2cd46f14260b4c42ad24d00fc19c
MgRafaele <32337931+MgRafaele@users.noreply.github.com>
2018-04-11 11:49:29 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -3,4 +3,21 @@ package Hosteleria.model;
 
 public class Menu {
 
+    private String nombre;
+    private double precio
+    private Plato primerPlato;
+    private Plato segundoPlato;
+    private Bebida bebida;
+    private Postre postre;
+  
+    public Menu(String nombre, double precio, Plato primerPlato, Plato segundoPlato, Bebida bebida, Postre postre) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.primerPlato = primerPlato;
+        this.segundoPlato = segundoPlato;
+        this.bebida = bebida;
+        this.postre = postre;
+    }  
+  
+  
 }
